ea7137902a9f072ed2b2c17d12c3303453c29bc2
Robert Deusser <iamthemuffinman@outlook.com>
2014-12-29 01:52:47 -0500

    Added jquery and package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "An updated Jquery package for Meteor",
+  git: "https://github.com/iamthemuffinman/meteor-jquery",
+  version: "0.0.1",
+  name: "iamthemuffinman/meteor-jquery"
+});
+
+Package.on_use(function(api, where) {
+  api.add_files("jquery-.1.11.2.min.js", "client");
+});
